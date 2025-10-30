3a8f701a383b6356cfd2187c69da302472778f69
GUAN Wenhao <68948269+gwh22@users.noreply.github.com>
2025-10-30 19:43:54 +0800

    Update infer_asr.sh

--- scripts/infer_asr.sh
+++ scripts/infer_asr.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=.
-CUDA_VISIBLE_DEVICES=0 torchrun --master_port 36507 --nproc_per_node 1 infer_small_f5_ema/infer_asr.py \
+CUDA_VISIBLE_DEVICES=0 torchrun --master_port 36507 --nproc_per_node 1 infers/infer_asr.py \
     --ckpt_path /ckpts/univoice_all \
     --llm_path /ckpts/SmolLM2-360M \
     --temperature 0.7 \
