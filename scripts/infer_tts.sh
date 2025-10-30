67c9b8a856eefd1589e1b25bc334b8de61d12645
GUAN Wenhao <68948269+gwh22@users.noreply.github.com>
2025-10-30 19:43:36 +0800

    Update infer_tts.sh

--- scripts/infer_tts.sh
+++ scripts/infer_tts.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=.
 
-CUDA_VISIBLE_DEVICES=0 torchrun --master_port 36505 --nproc_per_node 1 infer_small_f5_ema/infer_tts_single_bigvgan.py \
+CUDA_VISIBLE_DEVICES=0 torchrun --master_port 36505 --nproc_per_node 1 infers/infer_tts.py \
     --ckpt_path  /ckpts/univoice_all \
     --llm_path /ckpts/SmolLM2-360M \
     --cfg_scale 2 \
